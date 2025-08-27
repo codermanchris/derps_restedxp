fc48eca69e069b8c2d0e077927e64eb996da4d07
Christopher Tower <ctower.coder@gmail.com>
2025-08-27 12:47:34 -0400

    1. Initial commit. Fully working. No position saves. No config file.

---
+++ core.lua
@@ -0,0 +1,154 @@
+-- constants
+local TENT_JUICE_FULL = "TENT JUICE FULL!"
+local FIND_TENT = "FIND A TENT!"
+local PLAYER = "player"
+
+-- local members
+local addonFrame = nil
+local isVisible = true
+local updateTimer = 0
+local lastValue = 0
+local lastMaxXp = 0
+
+-- local functions
+local function CreateAddonFrame()
+    -- create frame
+    addonFrame = CreateFrame("Frame", "Derps Rested XP", UIParent)    
+    addonFrame:SetHeight(50)
+	addonFrame:SetWidth(300)
+    addonFrame:SetPoint("CENTER", UIParent)
+
+    -- setup background
+    addonFrame:SetBackdrop({
+        bgFile = "Interface/Tooltips/UI-Tooltip-Background",
+        tile = true, tileSize = 10, edgeSize = 10,
+        insets = { left = 4, right = 4, top = 4, bottom = 4 }
+    })
+    addonFrame:SetBackdropColor(0, 0, 0, 0.8)
+
+    -- Make it draggable
+    addonFrame:SetMovable(true)
+    addonFrame:EnableMouse(true)
+    addonFrame:RegisterForDrag("LeftButton")
+    
+    -- text label
+    local text = addonFrame:CreateFontString(nil, "ARTWORK", "GameFontNormal")
+    text:SetPoint("CENTER", addonFrame, "CENTER")
+    text:SetTextColor(1, 1, 1, 1)
+    text:SetFont("Fonts\\FRIZQT__.TTF", 12, "OUTLINE")
+    text:SetText(FIND_TENT)
+
+    -- frame events
+    addonFrame:SetScript("OnDragStart", function()
+        this:StartMoving()
+    end)
+    
+    addonFrame:SetScript("OnDragStop", function()
+        this:StopMovingOrSizing()
+    end)
+
+    addonFrame:SetScript("OnUpdate", function()
+        local self = this
+        local elapsed = arg1
+
+        -- init updateTimer
+        if not self.updateTimer then
+            self.updateTimer = 0
+        end
+
+        -- update elapsed time
+        self.updateTimer = self.updateTimer + elapsed
+
+        -- if we're passed one second we want to do some stuff
+        if self.updateTimer >= 1 then
+            -- get character xp information
+            local currentXp = UnitXP(PLAYER);
+            local maxXp = UnitXPMax(PLAYER);
+            local restedXp = GetXPExhaustion();
+
+            -- if the character levels, we don't want a huge jump in last kill %
+            if (maxXp ~= lastMaxXp and lastMaxXp ~= 0) then
+                -- init or new level
+                maxXp = lastMaxXp
+            end
+            lastMaxXp = maxXp
+
+            -- if the character has no rested xp
+            if (restedXp or -1) == -1 then
+                -- if we have a value change update the ui
+                if (lastValue ~= 0) then
+                    lastValue = 0
+
+                    -- notify the player by sound
+                    PlaySound("igQuestFailed", "master")
+
+                    -- set the text label 
+                    text:SetText(FIND_TENT)
+
+                    -- we want to show the UI if it's not shown when we hit 0 rested
+                    if (not isVisible) then
+                        isVisible = true
+                        self:Show()
+                    end
+                end
+            else -- the user has rested xp
+
+                -- caluclate rested xp percent
+                local value = 100 * restedXp / maxXp
+
+                -- if we have a value change update the ui
+                if (lastValue ~= value) then
+                    -- figure out the difference to display gain/loss rate
+                    local difference = value - lastValue
+                    lastValue = value
+
+                    -- if we're at capacity tell the user
+                    if (value >= 150) then
+                        -- inform the user that the tent juice is full
+                        text:SetText(TENT_JUICE_FULL)
+
+                        --notify the user by sound
+                        PlaySound("LEVELUPSOUND", "master")
+                    else
+                        if (difference > 0.09 and difference ~= value) then
+                            text:SetText(string.format("Tent Juice: %.1f%% | Gain Rate: %.1f%%", value, difference))
+                        elseif (difference < 0.09 and difference ~= value) then
+                            text:SetText(string.format("Tent Juice: %.1f%% | Last Kill: %.1f%%", value, difference))
+                        else
+                            text:SetText(string.format("Tent Juice: %.1f%%", value))
+                        end
+                    end
+                end
+            end
+
+            -- clear the timer for the next iteration
+            self.updateTimer = 0
+        end
+    end)
+end
+
+local function TJCommandHandler(msg, editBox)
+    isVisible = not isVisible
+
+    if (isVisible) then
+        addonFrame:Show()
+    else
+        addonFrame:Hide()
+    end
+end
+
+-- event frame
+local eventFrame = CreateFrame("Frame")
+eventFrame:RegisterEvent("ADDON_LOADED")
+
+eventFrame:SetScript("OnEvent", function()
+	if (event == "ADDON_LOADED") then
+		CreateAddonFrame()
+
+		this:UnregisterEvent("ADDON_LOADED")
+	end
+end)
+
+-- handle clash command
+SLASH_TJ1 = "/tj"
+SlashCmdList["TJ"] = TJCommandHandler
